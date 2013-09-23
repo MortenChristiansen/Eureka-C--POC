87ea8489dd4d3df044d74d3d0d5bf01e1127ba4d
clone1985 <withoutprecedence@gmail.com>
2013-09-23 21:01:02 +0200

    Added F# test code

---
+++ Eureka.CSharp.Poc/FSharpTest/Library1.fs
@@ -0,0 +1,32 @@
+﻿namespace FSharpTest
+
+open ServiceStack.ServiceInterface
+open ServiceStack.Common
+open ServiceStack.ServiceHost
+
+[<CLIMutable>]
+type Company = { mutable Id:string; mutable Name:string }
+
+[<Route("/companies", "POST")>]
+type CreateCompany() =
+    interface IReturn<Company>
+    member val Id = "" with get, set
+    member val Name = "" with get, set
+
+[<Route("/companies", "PUT")>]
+type UpdateCompany() =
+    interface IReturn<Company>
+    member val Id = "" with get, set
+    member val Name = "" with get, set
+
+type CompaniesService() =
+    inherit Service()
+
+    member this.Post (request:CreateCompany) : Company =
+        let company = request.TranslateTo<Company>()
+        company
+
+    member this.Put (request:UpdateCompany) : Company =
+        let company = { Id = "id"; Name = "Company" }
+        company.PopulateWith(request) |> ignore
+        company
